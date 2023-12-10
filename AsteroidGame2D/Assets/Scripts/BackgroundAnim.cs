2ef7fc7f87e80847d66b60f4dff7614cc61abaa5
Diyor Ikromov <diyorzed@gmail.com>
2023-12-10 21:17:44 +0100

    Simple changes
    Added sprites of the game
    Added simple movement of bg to the bottom
    Added spaceship and it's VFX using particle system

---
+++ AsteroidGame2D/Assets/Scripts/BackgroundAnim.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundAnim : MonoBehaviour
+{
+    [Range(0.1f, 3f)][SerializeField] private float speed;
+    [SerializeField] private Vector3 movementSide;
+    private Vector3 _startPos;
+    private Vector3 _endPos;
+    private float _distance;
+    void Start()
+    {
+        _endPos.x = GetComponent<SpriteRenderer>().sprite.rect.width / 100.0f;
+        _endPos.y = GetComponent<SpriteRenderer>().sprite.rect.height / 100.0f;
+        _startPos = transform.position;
+        
+        Debug.Log(_endPos);
+    }
+
+    void Update()
+    {
+        if (Mathf.Abs(transform.position.x) < Mathf.Abs(_endPos.x) && Mathf.Abs(transform.position.y) < Mathf.Abs(_endPos.y))
+        {
+            transform.position += movementSide * (speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = _startPos;
+        }
+    }
+}
